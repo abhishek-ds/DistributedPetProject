63759c01014057b4865b2d898707a2588b735c22
Abhishek <abhishekds1992@gmail.com>
2017-04-27 17:16:14 -0400

    Initial Project Setup

---
+++ DistributedPetProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DistributedPetProject.Startup))]
+namespace DistributedPetProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
