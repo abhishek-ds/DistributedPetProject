068bc15eb67f88da8425548fe76cfed8271ee18b
Abhishek <abhishekds1992@gmail.com>
2017-05-16 19:50:36 -0400

    Adding a Database connection framework

--- DistributedPetProject/Controllers/HomeController.cs
+++ DistributedPetProject/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.Mvc;
 
 namespace DistributedPetProject.Controllers
 {
@@ -23,6 +25,25 @@ namespace DistributedPetProject.Controllers
         // To be implemented
         public ActionResult Dashboard()
         {
+            //need to add database name passing connection string
+            string connectionString = "";
+            SqlConnection con = new SqlConnection(connectionString);
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("slect * from dashboardtable");
+                con.Open();
+                //map the data to models
+                //modelsomething = sqlCommand.ExecuteReader();
+            }
+            catch
+            {
+                Console.WriteLine("error");
+            }
+            finally
+            {
+                con.Close();
+            }
+
             return View();
         }
     }
